142cef22f6d5c837e59390756878842bf6930c26
GauravFrontend <gaurav9911093928@gmail.com>
2024-01-03 22:30:54 +0530

    mega blog auth added

---
+++ megablog/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+
+  return (
+    <>jhgjhg</>
+  )
+}
+ 
+export default App
---
+++ megablog/src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf/conf.js"
+
+import { Clint, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Clint();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, passward, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, passward, name);
+            if (userAccount) {
+                return this.login({ email, passward });
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, passward }) {
+        try {
+            return await this.account.createEmailSession(email, passward);
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
