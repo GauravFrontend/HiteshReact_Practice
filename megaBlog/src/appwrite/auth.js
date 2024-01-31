d3401133025869fd623fda2b8d30e49affea28a3
GauravFrontend <gaurav9911093928@gmail.com>
2024-01-31 23:18:57 +0530

    making auth services

---
+++ megaBlog/src/appwrite/auth.js
@@ -0,0 +1,52 @@
+import { Client, Account, ID } from "appwrite"
+//class , constructor
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("some error in creating account")
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+
+
+
+
+}
+
+const authService = new AuthService();
+
+export default AuthService
+
