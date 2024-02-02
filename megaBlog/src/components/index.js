42ed4f911caea2cffb298320ade08c44e6f64b38
GauravFrontend <gaurav9911093928@gmail.com>
2024-02-03 03:09:48 +0530

    adding form and slug values 34

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -3,10 +3,18 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import RTE from "./RTE";
+import Input from "./Input"
+import Select from "./Select"
 export {
     Header,
     Footer,
     Container,
+    Button,
+    Input,
+    Select,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 } 
